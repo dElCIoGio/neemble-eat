48b240824cc2506277e72a0ed22cc9d30108e3be
Delcio <dagostinho4@gmail.com>
2024-11-12 20:18:20 +0000

    styled the landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,26 @@ import Background from "@/components/ui/Background.tsx";
 import {useIsMobile} from "@/hooks/use-mobile.tsx";
 import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
 import {AlignCenter} from "lucide-react"
+// import background from "@/../public/images/blackchef2.png";
+import panel from "@/../public/images/dashboard.png";
+import {QrCode, ChartNoAxesCombined, ChefHat} from "lucide-react"
+
 
 export function Home() {
 
 	const isMobile = useIsMobile()
 
 	return (
-		<div className="h-screen bg-cover">
-			<Background className="bg-gradient-to-br from-slate-50 to-amethyst-800"/>
+		<div className="h-screen bg-cover" >
+			{/*style={{backgroundImage: `url(${background})`}}*/}
+
+			<Background className="bg-gradient-to-br from-slate-50 to-zinc-300"/>
+			{/*<Background className="bg-gradient-to-br from-slate-50 to-amethyst-900"/>*/}
 			<div className="w-full">
 				<nav className="p-4 bg-opacity-50 flex justify-between items-center">
 					<h1 className="text-xl font-bold">NEEMBLE EAT</h1>
 					{isMobile ? <Sheet>
-						<SheetTrigger asChild><AlignCenter/></SheetTrigger>
+							<SheetTrigger asChild><AlignCenter/></SheetTrigger>
 							<SheetContent className="py-12">
 
 								<div className="flex flex-col gap-3">
@@ -37,33 +44,45 @@ export function Home() {
 									Login
 								</Link>
 							</Button>
-							<Button asChild className="rounded-full">
+							<Button asChild className="rounded-2xl px-5">
 								<Link to={`${URL_PATH_PREFIX}/signup`} className="border-white">
 									Criar Conta
 								</Link>
 							</Button>
 
-					</div>
+						</div>
 					}
 
 				</nav>
-				<main className="max-w-[920px] w-full mx-auto py-16 px-4">
+				<main className="max-w-[920px] w-full mx-auto py-12 px-4">
 					<div className="text-center">
-						<h1 className="text-6xl font-bold">
-							Aumente o seu rendimento e performance com pedidos online
+						<h1 className="text-4xl laptop:text-6xl font-bold">
+							Aumente o seu  rendimento e performance com <span className="bg-gradient-to-r from-violet-600 to-indigo-600 text-transparent inline-block bg-clip-text">pedidos
+							online</span>
 						</h1>
-						<h2 className={"text-base mt-8 max-w-[700px] mx-auto text-gray-500"}>
+						<h2 className={"text-sm laptop:text-base mt-8 max-w-[700px] mx-auto text-gray-500"}>
 							A nossa plataforma oferece uma solução robusta para automatizar as operações do seu
 							restaurante e promover uma experiência ultra dinâmica para os seus clientes.
 						</h2>
 					</div>
 					<div className="mt-8 mx-auto flex justify-center gap-4">
-						<Button asChild className="rounded-full px-6 shadow-lg">
+						<Button asChild className="rounded-full px-6 shadow-lg bg-gradient-to-br hover:shadow-xl hover:-translate-y-1 from-black to-black hover:from-black hover:to-amethyst-300 transition-all duration-300">
 							<Link to={`${URL_PATH_PREFIX}/signup`} className="border-white">
 								Comece agora e ganhe 10% de desconto
 							</Link>
 						</Button>
 					</div>
+					<div className="mt-12">
+						<div className="flex justify-between space-x-4 my-2">
+							<QrCode className="bg-white p-1 text-amethyst-600 shadow-sm bg-opacity-30 rounded-md"/>
+							<ChartNoAxesCombined className="bg-white p-1 text-amethyst-600 shadow-sm bg-opacity-30 rounded-md"/>
+							<ChefHat className="bg-white p-1 text-amethyst-600 shadow-sm bg-opacity-30 rounded-md"/>
+						</div>
+						<img src={panel}
+							 className=""
+							 alt=""/>
+					</div>
+
 				</main>
 			</div>
 		</div>
