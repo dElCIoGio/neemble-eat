4592c1dae0d6040c8ec854d66e0fbda46162e5ba
Delcio <dagostinho4@gmail.com>
2024-11-18 14:21:19 +0000

    updated the GET_RESTAURANT endpoint link

--- src/api/restaurant/endpoints.ts
+++ src/api/restaurant/endpoints.ts
@@ -4,6 +4,6 @@ const ROUTER = "/restaurants"
 
 export const ENDPOINTS = {
     GET_RESTAURANTS_TOP_ORDERS: (restaurantId: string) => `${ROUTER}/${restaurantId}/last-orders`,
-    GET_RESTAURANT: (restaurantId: string) => `${ROUTER}/${restaurantId}/`,
+    GET_RESTAURANT: (restaurantId: string) => `${ROUTER}/${restaurantId}`,
     GET_ALL_TABLES: (restaurantId: string) => `${ROUTER}/${restaurantId}/all-tables`,
 }
--- src/api/utils.ts
+++ src/api/utils.ts
@@ -14,10 +14,10 @@ export const baseConfig: AxiosRequestConfig = {
 
 export const BASE_URL = online ? CloudRunUrl : localhostUrl
 
-// const protocol = `${online ? "https:" : "http:"}//`
+const protocol = `${online ? "https:" : "http:"}//`
 
 export const API = axios.create({
-    baseURL: `//${BASE_URL}`
+    baseURL: `${protocol}${BASE_URL}`
 })
 
 export async function handleError(error: AxiosError) {
